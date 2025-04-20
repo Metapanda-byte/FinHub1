e9054a70c3fae409a630355c4ef3ec99f4a853f4
Metapanda-byte <grblehaut@gmail.com>
2025-04-20 11:25:03 +0800

    Commit from Bolt to GitHub

--- next.config.js
+++ next.config.js
@@ -1,13 +1,36 @@
 /** @type {import('next').NextConfig} */
+const TerserPlugin = require('terser-webpack-plugin');
+
 const nextConfig = {
   output: 'export',
   images: {
-    domains: ['images.pexels.com'],
+    domains: ['images.pexels.com', 'financialmodelingprep.com'],
     unoptimized: true
   },
+  typescript: {
+    ignoreBuildErrors: true
+  },
   eslint: {
-    ignoreDuringBuilds: true,
-  }
+    ignoreDuringBuilds: true
+  },
+  trailingSlash: true,
+  swcMinify: false,
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.optimization.minimize = true;
+      config.optimization.minimizer = [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true,
+            },
+            mangle: true,
+          },
+        }),
+      ];
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
